5ba358e35ced188e214e1d4df41e22d3e0439184
Amit-Sheer Cohen <amitcohen95@gmail.com>
2020-11-25 14:29:33 +0200

    added tests base

---
+++ src/tests/BaseTest.java
@@ -0,0 +1,48 @@
+package tests;
+
+
+import api.DWGraph_DS;
+import api.NodeData;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.*;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BaseTest {
+    static DWGraph_DS graph;
+    static FileOutputStream f;
+
+    @BeforeAll
+    static void mainSetup() throws IOException {
+//        File file = new File(path+"/writeOver.json");
+//        file.delete();
+//        file.createNewFile();
+//        file = new File(path+ "/writeOver.txt");
+//        file.delete();
+//        file.createNewFile();
+    }
+
+    @BeforeEach
+    void setUp() {
+        graph = new DWGraph_DS();
+    }
+
+    public void graphCreator(int seed, int size, int edges, int weight){
+        Random rnd = new Random(seed);
+        Random rndWeight = new Random(weight);
+        Random keyRandom = new Random(seed);
+        for (int i = 0; i < size; i++) {
+            NodeData n = new NodeData(i);
+            graph.addNode(n);
+        }
+        int key,key2;
+        while(graph.edgeSize()< edges) {
+            key = keyRandom.nextInt(size);
+            key2 = keyRandom.nextInt(size);
+            graph.connect(key2, key, rndWeight.nextDouble());
+        }
+    }
+}
---
+++ src/tests/DWGraph_DSTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DWGraph_DSTest extends BaseTest {
+
+    @Test
+    void getNode() {
+        graphCreator(1,10,0,0);
+
+    }
+
+    @Test
+    void getEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addNode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void connect() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getV() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getE() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeNode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nodeSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void edgeSize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMC() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
